cb3806fbcc2ae2dcd2eca4ae76b0621a7c55fa22
mildosky <ibrahimmusah205@gmail.com>
2023-04-18 16:52:28 +0100

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,37 +1,34 @@
 #!/usr/bin/python3
-"""This is the user class"""
-from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship
+"""This module defines a class User"""
+import os
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
-s = "HBNB_TYPE_STORAGE"
-if cs in environ.keys() and environ["HBNB_TYPE_STORAGE"] == "db":
-    class User(BaseModel, Base):
-        """This is the class for user
-        Attributes:
-        email: email address
-        password: password for you login
-        first_name: first name
-        last_name: last name
-        """
-        __tablename__ = "users"
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user")
-        reviews = relationship("Review", backref="user")
+from models.base_model import BaseModel, Base
 
-        def __init__(self, **kwargs):
-            setattr(self, "id", str(uuid4()))
-            for i, j in kwargs.items():
-                setattr(self, i, j)
-else:
-    class User(BaseModel):
-        """This is the class for user
-        Attributes:
-        """
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+
+class User(BaseModel, Base):
+    """This class defines a user by various attributes"""
+    __tablename__ = 'users'
+    email = Column(
+        String(128), nullable=False
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    password = Column(
+        String(128), nullable=False
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    first_name = Column(
+        String(128), nullable=True
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    last_name = Column(
+        String(128), nullable=True
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    places = relationship(
+        'Place',
+        cascade="all, delete, delete-orphan",
+        backref='user'
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else None
+    reviews = relationship(
+        'Review',
+        cascade="all, delete, delete-orphan",
+        backref='user'
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else None
