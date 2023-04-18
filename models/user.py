ef4bd38fc5ad4438c333c47adb8a8f1d2e286766
mildosky <ibrahimmusah205@gmail.com>
2023-04-18 16:45:21 +0100

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,29 +1,37 @@
 #!/usr/bin/python3
-""" holds class User"""
-import models
+"""This is the user class"""
 from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
 
-
-class User(BaseModel, Base):
-    """Representation of a user """
-    if models.storage_t == 'db':
-        __tablename__ = 'users'
+s = "HBNB_TYPE_STORAGE"
+if cs in environ.keys() and environ["HBNB_TYPE_STORAGE"] == "db":
+    class User(BaseModel, Base):
+        """This is the class for user
+        Attributes:
+        email: email address
+        password: password for you login
+        first_name: first name
+        last_name: last name
+        """
+        __tablename__ = "users"
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
         places = relationship("Place", backref="user")
         reviews = relationship("Review", backref="user")
-    else:
+
+        def __init__(self, **kwargs):
+            setattr(self, "id", str(uuid4()))
+            for i, j in kwargs.items():
+                setattr(self, i, j)
+else:
+    class User(BaseModel):
+        """This is the class for user
+        Attributes:
+        """
         email = ""
         password = ""
         first_name = ""
         last_name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes user"""
-        super().__init__(*args, **kwargs)
