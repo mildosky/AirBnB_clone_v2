ca55c6ee02d31ad19abed48096de317f6cc409ae
mildosky <ibrahimmusah205@gmail.com>
2023-04-18 16:51:47 +0100

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,37 +1,21 @@
 #!/usr/bin/python3
-"""This is the review class"""
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey, Integer, Float, Table
+""" Review module for the HBNB project """
+import os
+from sqlalchemy import Column, ForeignKey, String
 from sqlalchemy.orm import relationship
-from os import environ
-from uuid import uuid4
 
-s = "HBNB_TYPE_STORAGE"
-if s in environ.keys() and environ["HBNB_TYPE_STORAGE"] == "db":
-    class Review(BaseModel, Base):
-        """This is the class for Review
-        Attributes:
-        place_id: place id
-        user_id: user id
-        text: review description
-        """
-        __tablename__ = "reviews"
-        text = Column(String(1024), nullable=False)
-        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
-        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+from models.base_model import BaseModel, Base
 
-        def __init__(self, **kwargs):
-            setattr(self, "id", str(uuid4()))
-            for i, j in kwargs.items():
-                setattr(self, i, j)
-else:
-    class Review(BaseModel):
-        """This is the class for Review
-        Attributes:
-            place_id: place id
-            user_id: user id
-            text: review description
-        """
-        place_id = ""
-        user_id = ""
-        text = ""
+
+class Review(BaseModel, Base):
+    """ Review classto store review information """
+    __tablename__ = 'reviews'
+    place_id = Column(
+        String(60), ForeignKey('places.id'), nullable=False
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    user_id = Column(
+        String(60), ForeignKey('users.id'), nullable=False
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    text = Column(
+        String(1024), nullable=False
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
