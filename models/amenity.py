ca693e8f8b7afcba3b296fc5872a34caa0bf2e26
Mercy Ndirangu <ndirangumercy4@gmail.com>
2023-04-19 12:59:25 -0700

    Revert "amenity"
    This reverts commit 0a2b548783b8961b8d6a1ca4b0b305bae6e36b9d.

--- models/amenity.py
+++ models/amenity.py
@@ -12,4 +12,15 @@ class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
     name = Column(
         String(128), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else"
+    )
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        place_amenities = relationship(
+                "Place", secondary="place_amenity", back_populates="amenities")
+        place_amenity = Table(
+                "place_amenity",
+                Base.metadata,
+                Column("place_id", String(60), ForeignKey("places.id"),
+                       primary_key=True, nullable=False),
+                Column("amenity_id", String(60), ForeignKey("amenities.id"),
+                       primary_key=True, nullable=False)
+                )
