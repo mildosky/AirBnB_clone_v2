b02151a7418119ac17fc2d42b94e60e4bb6dcd1b
mildosky <ibrahimmusah205@gmail.com>
2023-05-04 14:58:07 +0100

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,38 +1,17 @@
 #!/usr/bin/env bash
-# Sets up a web server for deployment of web_static.
-
-apt-get update
-apt-get install -y nginx
-
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
+# Script that configures Nginx server with some folders and files
+sudo apt-get -y update
+sudo apt-get -y install nginx
+sudo service nginx stop
+sudo service nginx start
+sudo mkdir -p /data/
+sudo chown -R ubuntu:ubuntu /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
 echo "Holberton School" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
-
-printf %s "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $HOSTNAME;
-    root   /var/www/html;
-    index  index.html index.htm;
-
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
-
-    location /redirect_me {
-        return 301 http://cuberule.com/;
-    }
-
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-}" > /etc/nginx/sites-available/default
-
-service nginx restart
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\n\tlocation \/hbnb_static\/ {\n\t\talias  \/data\/web_static\/current\/;\n\t\tautoindex off;\n\t}/" /etc/nginx/sites-enabled/default
+sudo service nginx restart
+exit 0
